a8cc6d10f876c4d8d4d0e236b1b560d5820683e6
Jasper Vogelzang <jasper.vogelzang@hotmail.nl>
2021-03-14 21:44:10 +0100

    Update Bioinformatics.R

--- Bioinformatics.R
+++ Bioinformatics.R
@@ -1,1 +1,5 @@
 #Bioinformatics 11-3
+exprsVijver <- read_delim("Documents/ADS Master/Routine Care/Lab Sessions/exprsVijver.txt", 
+                          "\t", escape_double = FALSE, trim_ws = TRUE)
+
+
