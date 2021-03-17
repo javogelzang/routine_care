8c3b46a8803de78f4e68768934412beae72f9fec
Jasper Vogelzang <jasper.vogelzang@hotmail.nl>
2021-03-17 21:55:53 +0100

    Update Bioinformatics.R

--- Bioinformatics.R
+++ Bioinformatics.R
@@ -4,16 +4,43 @@ library(caret)
 library(boot)
 library(affy)
 
+#Load the data 
 setwd("Documents/ADS Master/Routine Care/Lab Sessions")
-df_expr <- read.delim("exprsVijver.txt")
-df_phen <- read.delim("pdatavijver.txt")
+df_expr <- read.table("exprsVijver.txt", sep = '\t', header = TRUE,
+                      check.names = FALSE)
+df_expr <- as.matrix(df_expr)
+df_phen <- read.table("pdatavijver.txt", sep = '\t', header = TRUE,
+                      check.names = FALSE)
 description <- read.table('pdataVijver.txt', sep = '\t')
 
-df_expr_t <- t(df_expr)
-y <- df_phen$event_death
-analysis_frame <- data.frame(df_expr_t,y=as.factor(y))
+#Check whether the order of samples in the expression data is the same as in the phenodata
+table(colnames(df_expr)==row.names(df_phen))
 
-x_features <- model.matrix(y ~ ., analysis_frame)
+#Make dataframe for use in the caret package
+vijverdata <- as.data.frame(t(df_expr))
+vijverdata$event_death <- df_phen$event_death
+
+#Lasso penalised logistic regression using glmnet
+lasso.fit <- glmnet(x = t(df_expr), y = vijverdata$event_death, family = 'binomial', alpha = 1)
+plot(lasso.fit)
+
+#Ridge penalised logistic regression using glmnet
+ridge.fit <- glmnet(x = t(df_expr), y = vijverdata$event_death, family = 'binomial', alpha = 0)
+plot(ridge.fit)
+
+#Cross-validation k-fold lasso to find optimal lambda
+cvlasso <- cv.glmnet(x = t(df_expr), y = vijverdata$event_death, family = 'binomial',
+                         keep = TRUE, alpha = 1)
+plot(cvlasso)
+
+#Cross-validation k-fold ridge to find optimal lambda
+cvridge <- cv.glmnet(x = t(df_expr), y = vijverdata$event_death, family = 'binomial',
+                         keep = TRUE, alpha = 0)
+plot(cvridge)
+
+#Select genes with optimal lambda
+beta.min <- cvlasso$glmnet.fit$beta[,cvlasso$index["min",]]
+beta.min[beta.min!=0]
 
 cv.lasso <- cv.glmnet(x - x_features, y - analysis_frame$y, alpha - 1, family = 'binomial')
 coef(cv.lasso, cv.lasso$lambda.min)
